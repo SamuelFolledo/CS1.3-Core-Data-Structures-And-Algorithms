bee5a46c80925d700252731f3d85cc24505bc63d
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-03 20:51:07 -0400

    [add] made possible characters of type Characters instead of string

--- classwork/NumbersConverter/NumbersConverter/Models/Base.swift
+++ classwork/NumbersConverter/NumbersConverter/Models/Base.swift
@@ -8,14 +8,14 @@
 
 import Foundation
 
-let kPOSSIBLECHARACTERS: [String] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let kPOSSIBLECHARACTERS: [Character] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 enum Base: Int {
     case two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, eight = 8, nine = 9, ten = 10, eleven = 11, twelve = 12, thirteen = 13, fourteen = 14, fifteen = 15, sixteen = 16, seventeen = 17, eighteen = 18, nineteen = 19, twenty = 20, twentyOne = 21, twentyTwo = 22, twentyThree = 23, twentyFour = 24, twentyFive = 25, twentySix = 26, twentySeven = 27, twentyEight = 28, twentyNine = 29, thirty = 30, thirtyOne = 31, thirtyTwo = 32, thirtyThree = 33, thirtyFour = 34, thirtyFive = 35, thirtysix = 36
     
 ///returns list of possible characters for that base
-    func getImpossibleCharacters() -> [String] {
-        var impossibleChars: [String] = []
+    func getImpossibleCharacters() -> [Character] {
+        var impossibleChars: [Character] = []
         let iterationCount = Base.allCases.count - self.rawValue + 1 //get length impossible characters
         for i in 0 ..< iterationCount {
             impossibleChars.append(kPOSSIBLECHARACTERS.reversed()[i]) //append each impossible characters //reversed to get impossible instead
