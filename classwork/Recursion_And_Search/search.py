32d173c5441d71f0f16b55317a10dffdfa084217
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-05 00:42:56 -0400

    [add] completed and passed all linear_search_recursive tests

--- classwork/Recursion_And_Search/search.py
+++ classwork/Recursion_And_Search/search.py
@@ -21,12 +21,12 @@ def linear_search_recursive(array, item, index=0):
     # pass
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
-    if index == len(array-1):
+    if index == len(array):
         return None
-    elif item == array[index]:
+    if item == array[index]:
         return index
     else:
-        linear_search_recursive(array, item, index+1)
+        return linear_search_recursive(array, item, index+1)
 
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
