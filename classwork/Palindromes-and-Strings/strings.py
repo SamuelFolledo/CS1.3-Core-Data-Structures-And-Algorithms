7aa205775858a08a9bbbe4ee16d8e837aed5e63e
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-12 02:30:00 -0400

    [add] made find_index iterative and recursive cleaner

--- classwork/Palindromes-and-Strings/strings.py
+++ classwork/Palindromes-and-Strings/strings.py
@@ -55,19 +55,19 @@ def find_index(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_index here (iteratively and/or recursively)
-    # index = find_index_iteratively(text, pattern)
-    index = find_index_recursively(text, pattern)
-    print(f"\n'{pattern}' is in {text} at index {index}")
-    return index
-
-def find_index_iteratively(text, pattern):
-    # if pattern not in text: #makes sure the pattern is in text
-    #     return None
+    #1) Turn text and pattern into array which only contains letters and spaces
     text_arr = "".join(c.lower() for c in text if c.isalpha() or c.isspace()) #turn text to array of strings which only contains alpha and whitespace characters (no numbers, symbols)
     pattern_arr = "".join(c.lower() for c in pattern if c.isalpha() or c.isspace()) #turn patterns to array
+    #2) Handle empty pattern_arr
+    if len(pattern_arr) == 0:
+        return 0 #return first index
+    #3) Look for first index of pattern in text
+    # index = find_index_iteratively(text_arr, pattern_arr)
+    index = find_index_recursively(text_arr, pattern_arr)
+    return index
+
+def find_index_iteratively(text_arr, pattern_arr):
     pattern_index = 0
-    if len(pattern_arr) == 0: #if pattern_arr is empty, then it's every index, but first is 0
-        return 0 #first index
     for i in range(len(text_arr)): #loop through each char in arr
         if text_arr[i] != pattern_arr[pattern_index]: #check if characters does not match, reset pattern_index
             pattern_index = 0
@@ -77,11 +77,7 @@ def find_index_iteratively(text, pattern):
                 return i + 1 - pattern_index #return (i + 1) - pattern_index
     return None
 
-def find_index_recursively(text, pattern, text_index=0, pattern_index=0):
-    text_arr = "".join(c.lower() for c in text if c.isalpha() or c.isspace()) #turn text to array of strings which only contains alpha and whitespace characters (no numbers, symbols)
-    pattern_arr = "".join(c.lower() for c in pattern if c.isalpha() or c.isspace()) #turn patterns to array
-    if len(pattern_arr) == 0: #if pattern_arr is empty, then it's every index, but first is 0
-        return 0
+def find_index_recursively(text_arr, pattern_arr, text_index=0, pattern_index=0):
     if text_index > len(text_arr) - 1: #if text_index go out of bounds, then we reached the end without finding index
         return None
     if text_arr[text_index] != pattern_arr[pattern_index]: #check if characters does not match, pattern_index = 0
@@ -90,7 +86,7 @@ def find_index_recursively(text, pattern, text_index=0, pattern_index=0):
         pattern_index += 1
         if pattern_index == len(pattern_arr): #if the entire pattern char matches, return True
             return text_index + 1 - pattern_index #return (i + 1) - pattern_index  
-    return find_index_recursively(text, pattern, text_index+1, pattern_index)
+    return find_index_recursively(text_arr, pattern_arr, text_index+1, pattern_index)
 
 def find_all_indexes(text, pattern):
     """Return a list of starting indexes of all occurrences of pattern in text,
@@ -157,23 +153,20 @@ def test_string_algorithms(text, pattern):
 
 def main():
     """Read command-line arguments and test string searching algorithms."""
-    # import sys
-    # args = sys.argv[1:]  # Ignore script file name
-    # if len(args) == 2:
-    #     text = args[0]
-    #     pattern = args[1]
-    #     test_string_algorithms(text, pattern)
-    # else:
-    #     script = sys.argv[0]
-    #     print('Usage: {} text pattern'.format(script))
-    #     print('Searches for occurrences of pattern in text')
-    #     print("\nExample: {} 'abra cadabra' 'abra'".format(script))
-    #     print("contains('abra cadabra', 'abra') => True")
-    #     print("find_index('abra cadabra', 'abra') => 0")
-    #     print("find_all_indexes('abra cadabra', 'abra') => [0, 8]")
-    indexes = find_all_indexes("abc", "z")
-    print(indexes)
-
+    import sys
+    args = sys.argv[1:]  # Ignore script file name
+    if len(args) == 2:
+        text = args[0]
+        pattern = args[1]
+        test_string_algorithms(text, pattern)
+    else:
+        script = sys.argv[0]
+        print('Usage: {} text pattern'.format(script))
+        print('Searches for occurrences of pattern in text')
+        print("\nExample: {} 'abra cadabra' 'abra'".format(script))
+        print("contains('abra cadabra', 'abra') => True")
+        print("find_index('abra cadabra', 'abra') => 0")
+        print("find_all_indexes('abra cadabra', 'abra') => [0, 8]")
 
 if __name__ == '__main__':
     main()
