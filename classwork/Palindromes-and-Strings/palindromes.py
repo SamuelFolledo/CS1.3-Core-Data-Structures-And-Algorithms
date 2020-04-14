34f37704565c74974130f4f58489055886e15ba9
Samuel Folledo <samuelfolledo@gmail.com>
2020-04-14 19:23:30 -0400

    [add] added day 5 updates

--- classwork/Palindromes-and-Strings/palindromes.py
+++ classwork/Palindromes-and-Strings/palindromes.py
@@ -17,6 +17,40 @@ def is_palindrome(text):
     return is_palindrome_recursive(text)
 
 
+# def is_palindrome_iterative(text):
+#     # TODO: implement the is_palindrome function iteratively here
+#     pass
+#     # once implemented, change is_palindrome to call is_palindrome_iterative
+#     # to verify that your iterative implementation passes all tests
+#     text_array = "".join(c.lower() for c in text if c.isalpha()) #turn text to array of strings which only contains alpha characters (no numbers, symbols, whitespaces)
+#     half = int(len(text_array) // 2 + (len(text_array) % 2 > 0)) #gets half length of array and round up if array length is odd in pure python way
+#     for index in range(half): #loop until halfway
+#         if text_array[index] != text_array[len(text_array) - 1 - index]:
+#             return False
+#     return True
+
+
+# def is_palindrome_recursive(text, left=None, right=None):
+#     # TODO: implement the is_palindrome function recursively here
+#     pass
+#     # once implemented, change is_palindrome to call is_palindrome_recursive
+#     # to verify that your iterative implementation passes all tests
+#     text_array = "".join(c.lower() for c in text if c.isalpha())
+#     half = int(len(text_array) // 2 + (len(text_array) % 2 > 0))
+#     if left == None:
+#         left = 0
+#     if right == None:
+#         right = len(text_array) - 1
+
+#     if left < half:
+#         if text_array[left] == text_array[right]:
+#             return is_palindrome_recursive(text, left+1, right-1)
+#         else:
+#             return False
+#     else:
+#         return True
+        
+
 def is_palindrome_iterative(text):
     # TODO: implement the is_palindrome function iteratively here
     pass
@@ -27,6 +61,14 @@ def is_palindrome_iterative(text):
     for index in range(half): #loop until halfway
         if text_array[index] != text_array[len(text_array) - 1 - index]:
             return False
+    #MARK: New Answer, Strings in Python is already an array
+    # left = 0
+    # right = len(text) - 1
+    # while left <= right:
+    #     if text[left] != text[right]:
+    #         return False
+    #     left += 1
+    #     right -= 1
     return True
 
 
@@ -49,8 +91,12 @@ def is_palindrome_recursive(text, left=None, right=None):
             return False
     else:
         return True
-        
-        
+    #MARK: New Answer
+    # if text[left] != text[right]:
+    #     return False
+    # if left >= right:
+    #     return True
+    # return is_palindrome_recursive(text, left+1, right-1)     
     
 
 
